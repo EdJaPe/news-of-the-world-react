c66e9e89eaf536688379d1f202bf84a19f5872cc
edJaPe <edv06jara@gmail.com>
2021-02-10 14:50:58 -0800

    api working

--- src/content/index.js
+++ src/content/index.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import axios from 'axios'
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import { Display } from './Display';
 import { Landing } from './Landing';
@@ -10,11 +11,23 @@ import '../styling/style.css';
 // rather than as a default
 // refer to index.js at src root to see how import works
 export const App = () => {
+    console.log(process.env)
+    const [content, setContent] = useState([])
+    var url = `http://newsapi.org/v2/top-headlines?`+`country=us&`+`apiKey=${process.env.REACT_APP_API_KEY}`
+    
+    useEffect(() => {
+        axios.get(url)
+        .then(res => {
+            setContent(res.data)
+        })
+    }, [] )
+    console.log(content)
+
     return (
         // Router for setting routes
         <Router>
             <div className='app'>
-                <Landing />
+                <Landing  />
                 <Display />
             </div>
         </Router>
